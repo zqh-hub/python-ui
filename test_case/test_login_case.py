d710ff5e89fcda28281ba9bfb04cf23b96554b71
zqh-hub <going163@163.com>
2021-04-05 16:55:31 +0800

    修改成功后的定位

--- test_case/test_login_case.py
+++ test_case/test_login_case.py
@@ -9,6 +9,7 @@ class TestLoginCase:
         access_web[1].submit_login(error_data["username"], error_data["password"])
         assert access_web[1].def_get_username_password_error_alert_text(), error_data["msg"]
 
+    @pytest.mark.success
     @pytest.mark.usefixtures("refresh_page")
     @pytest.mark.parametrize("success_data", login_data.success_login)
     def test_success_login(self, access_web, success_data):
